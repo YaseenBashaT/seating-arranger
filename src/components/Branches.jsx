61997cac270a3ba1f18544bd010fcd13a054d598
Anand <pasupuletimangapathi@gmail.com>
2024-09-09 22:40:30 +0530

    initial commit

---
+++ src/components/Branches.jsx
@@ -0,0 +1,8 @@
+import BranchTable from "./BranchTable"
+
+export default function Branches(){
+    return <main>
+        <h1>Branch details</h1>
+        <BranchTable />
+    </main>
+}
---
+++ src/components/Rooms.jsx
@@ -0,0 +1,8 @@
+import Roomstable from "./roomsTable"
+
+export default function Rooms(){
+    return <main>
+        <h1>rooms details</h1>
+        <Roomstable />
+    </main>
+}
